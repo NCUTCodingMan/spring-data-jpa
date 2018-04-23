648626c29797f2fc3ccdf5244cf5fbe31b836ce4
NCUTCodingMan <1667254420@qq.com>
2018-04-23 18:19:18 +0800

    修改事务管理器配置为JpaTransactionManager. 简单梳理Spring的事务管理器类型以及afterPropertiesSet()的作用

--- src/main/java/com/rain/config/SpringAppInitializer.java
+++ src/main/java/com/rain/config/SpringAppInitializer.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 /**
@@ -50,6 +54,7 @@ import javax.sql.DataSource;
  * created on 2018/4/22
  */
 @Configuration
+@EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"com.rain.repository"})
 public class SpringAppInitializer {
     private static final String USERNAME = "root";
@@ -83,31 +88,49 @@ public class SpringAppInitializer {
     }
 
     /**
-     * LocalContainerEntityManagerFactoryBean
+     * LocalContainerEntityManagerFactoryBean 实体类管理器
      * @param adapter Hibernate
      * @param dataSource DataSource
      * @return LocalContainerEntityManagerFactoryBean
      */
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(JpaVendorAdapter adapter, DataSource
+    public EntityManagerFactory entityManagerFactory(JpaVendorAdapter adapter, DataSource
             dataSource) {
         LocalContainerEntityManagerFactoryBean emfb = new LocalContainerEntityManagerFactoryBean();
         emfb.setDataSource(dataSource);
         emfb.setJpaVendorAdapter(adapter);
         // 设置扫描实体包
         emfb.setPackagesToScan("com.rain.entity");
-        return emfb;
+        /*
+        * afterPropertiesSet()
+        *   至于为什么需要调用一下这个方法
+        *   调用LocalContainerEntityManagerFactoryBean.afterPropertiesSet()很关键, 不调用的话, factory不会得到构建
+        *   继而getObject()将会返回null, 因而后面就会出现异常了
+        * */
+        emfb.afterPropertiesSet();
+        return emfb.getObject();
     }
 
     /**
      * 为什么需要配置TransactionManager...
-     * @param dataSource 数据源
+     *
+     * PlatformTransactionManager
+     *  事务管理器抽象, 对于不同的数据访问框架通过实现此策略接口, 从而能够实现各种数据访问框架的事务管理
+     *
+     * 这里简单介绍一下几个Spring内置事务管理器的实现
+     * 1.DataSourceTransactionManager
+     *  提供对单个DataSource的事务管理, 用于Spring JDBC, iBatis, MyBatis的事务管理
+     * 2.JpaTransactionManager
+     *  提供对单个EntityManagerFactory事务支持, 用于集成JPA实现框架时的事务管理
+     * 3.HibernateTransactionManager
+     *  用于Hibernate集成时的事务管理器
+     * @param entityManagerFactory 数据源
      * @return TransactionManager
      */
     @Bean
-    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
-        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
-        dataSourceTransactionManager.setDataSource(dataSource);
-        return dataSourceTransactionManager;
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
     }
 }
